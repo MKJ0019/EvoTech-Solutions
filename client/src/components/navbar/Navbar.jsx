22ef08882d354b30f6260a2b77bef364ee2dc51c
Mayank <mail.mayank001@gmail.com>
2024-08-06 21:18:58 +0530

    Update

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -50,6 +50,14 @@ export default function Navbar() {
             </li>
             <li>
               <Link
+                to="/about"
+                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link
                 to="/contact"
                 className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 aria-current="page"
@@ -63,14 +71,6 @@ export default function Navbar() {
                 className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
                 Meet with us
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/about"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-              >
-                About Us
               </Link>
             </li>
           </ul>
@@ -104,6 +104,15 @@ export default function Navbar() {
             </li>
             <li>
               <Link
+                to="/about"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
+                onClick={() => setIsOpen(false)}
+              >
+                About Us
+              </Link>
+            </li>
+            <li>
+              <Link
                 to="/contact"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                 onClick={() => setIsOpen(false)}
@@ -118,15 +127,6 @@ export default function Navbar() {
                 onClick={() => setIsOpen(false)}
               >
                 Make a Meet With Us
-              </Link>
-            </li>
-            <li>
-              <Link
-                to="/about"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
-                onClick={() => setIsOpen(false)}
-              >
-                About Us
               </Link>
             </li>
           </ul>
