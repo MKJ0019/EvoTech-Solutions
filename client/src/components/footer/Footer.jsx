faa95e7ee674e42f1bafa60de1fd02f1f1d39029
Mayank <mail.mayank001@gmail.com>
2024-08-06 21:49:59 +0530

    Update

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FiInstagram, FiLinkedin } from "react-icons/fi";
 import { IoLogoWhatsapp, IoMdCall } from "react-icons/io";
+import { TextField, Button } from "@mui/material";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 export default function Footer() {
-   const year = new Date().getFullYear()
+  const year = new Date().getFullYear();
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubscribe = async () => {
+    try {
+      if (email === "") {
+        toast.error("Email cannot be empty.");
+        return;
+      }
+      if (!email.includes("@")) {
+        toast.error("Please enter a valid email address.");
+        return;
+      }
+
+      setLoading(true);
+      const data = {
+        user_id: "DVqy7KTmBnMafnm2o",
+        service_id: "service_k4h1r6h",
+        template_id:  "template_ivim1a7",
+        template_params: {
+          to_name: "AeroWeb Solutions",
+          from_email: email,
+        },
+      };
+
+      const res = await axios.post(
+        "https://api.emailjs.com/api/v1.0/email/send",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(res.data);
+      toast.success("Subscription successful!");
+      setEmail(""); // Clear email input
+    } catch (e) {
+      console.error("Error sending email:", e);
+      toast.error("Failed to subscribe. Please try again.");
+    }
+    setLoading(false);
+  };
+
   return (
     <div className="bg-zinc-100">
       <footer className="max-w-[90%] mx-auto text-foreground p-6">
@@ -59,14 +106,25 @@ export default function Footer() {
           <div className="w-full md:w-auto flex flex-col items-start md:items-center">
             <h3 className="text-lg font-semibold mb-2">Newsletter</h3>
             <div className="flex w-full">
-              <input
+              <TextField
                 type="email"
-                placeholder="Your email here.."
-                className="border border-border rounded-l-md p-2 w-full md:w-auto flex-grow"
+                placeholder="Your email here..."
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                variant="outlined"
+                size="small"
+                fullWidth
+                className="border border-border rounded-l-md p-2"
               />
-              <button className="bg-gray-300 text-primary-foreground hover:bg-primary/80 rounded-r-md p-2 w-full md:w-auto">
-                Subscribe
-              </button>
+              <Button
+                onClick={handleSubscribe}
+                variant="contained"
+                color="primary"
+                disabled={loading}
+                className="rounded-r-md p-2"
+              >
+                {loading ? "Subscribing..." : "Subscribe"}
+              </Button>
             </div>
           </div>
         </div>
--- client/src/components/outlet/contact/Contact.jsx
+++ client/src/components/outlet/contact/Contact.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button } from "@mui/material";
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 export default function Contact() {
+  const [inputs, setInputs] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
+
+  const data = {
+    user_id: "DVqy7KTmBnMafnm2o",
+    service_id: "service_k4h1r6h",
+    template_id: "template_ivim1a7",
+    template_params: {
+      from_name: inputs.name,
+      from_email: inputs.email,
+      to_name: "AeroWeb Solutions",
+      message: inputs.message,
+    },
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+    try {
+      if (inputs.name === "" || inputs.email === "" || inputs.message === "") {
+        toast.error("All fields are required.");
+        return;
+      }
+      if (!inputs.email.includes("@")) {
+        toast.error("Please enter a valid email address.");
+        return;
+      }
+      
+      setLoading(true);
+      const res = await axios.post(
+        "https://api.emailjs.com/api/v1.0/email/send",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Response:", res.data);
+      toast.success("Email sent successfully.");
+      setInputs({ name: "", email: "", message: "" });
+    } catch (e) {
+      console.error("Error details:", e.response ? e.response.data : e.message);
+      toast.error("Error while sending email.");
+    }
+    setLoading(false);
+  };
+  
+
   return (
     <div>
       <section className="px-2 pt-[100px] py-12 lg:px-12 w-full">
@@ -17,13 +72,13 @@ export default function Contact() {
 
               <div>
                 <h3 className="text-xl font-semibold">Address:</h3>
-                <p>Kharar Chandigarh, Punjab India </p>
+                <p>Kharar Chandigarh, Punjab India</p>
               </div>
 
               <div>
                 <h3 className="text-xl font-semibold">Email:</h3>
                 <p>
-                  <a href="mailto:info@devndez.com" className="text-blue-500">
+                  <a href="mailto:info@aerotech.com" className="text-blue-500">
                     info@aerotech.com
                   </a>
                 </p>
@@ -32,7 +87,7 @@ export default function Contact() {
               <div>
                 <h3 className="text-xl font-semibold">Phone:</h3>
                 <p>
-                  <a href="tel:+91 8287758779" className="text-blue-500">
+                  <a href="tel:+91 9508359061" className="text-blue-500">
                     +91 9508359061
                   </a>
                 </p>
@@ -41,7 +96,7 @@ export default function Contact() {
 
             {/* Contact Form */}
             <div className="w-full md:w-[69%] p-6 rounded-lg -mt-0 md:-mt-10 lg:-mt-10">
-              <form className="space-y-4">
+              <form className="space-y-4" onSubmit={handleSubmit}>
                 <input type="hidden" name="access_key" />
 
                 <h2 className="text-blue-600 text-2xl font-semibold">
@@ -51,8 +106,7 @@ export default function Contact() {
                   Let’s Chat 👋 Consultation Is Free!
                 </h1>
                 <p className="text-lg text-gray-500 mb-7">
-                  Have questions or feedback? We're here to help. Send us a
-                  message, and we'll respond within 30 minutes.
+                  Have questions or feedback? We're here to help. Send us a message, and we'll respond within 30 minutes.
                 </p>
 
                 <div>
@@ -60,6 +114,8 @@ export default function Contact() {
                     label="Name"
                     name="name"
                     id="name"
+                    value={inputs.name}
+                    onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
                     required
                     variant="standard"
                     fullWidth
@@ -72,6 +128,8 @@ export default function Contact() {
                     label="Email"
                     name="email"
                     id="email"
+                    value={inputs.email}
+                    onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
                     required
                     type="email"
                     variant="standard"
@@ -85,6 +143,8 @@ export default function Contact() {
                     label="Message"
                     name="message"
                     id="message"
+                    value={inputs.message}
+                    onChange={(e) => setInputs({ ...inputs, message: e.target.value })}
                     required
                     multiline
                     rows={4}
@@ -108,8 +168,9 @@ export default function Contact() {
                     variant="contained"
                     color="primary"
                     className="mt-4"
+                    disabled={loading}
                   >
-                    Submit
+                    {loading ? "Sending..." : "Submit"}
                   </Button>
                 </div>
               </form>
@@ -117,19 +178,16 @@ export default function Contact() {
           </div>
 
           {/* Map Section */}
-          <div
-            className="mt-12 px-6 lg:px-0"
-            style={{ fontFamily: "Archivo, sans-serif" }}
-          >
+          <div className="mt-12 px-6 lg:px-0" style={{ fontFamily: "Archivo, sans-serif" }}>
             <h3 className="text-2xl font-bold mb-4">Find Us on the Map</h3>
             <div className="h-64 w-full rounded-lg shadow-md overflow-hidden">
               <iframe
                 width="100%"
                 height="100%"
-                frameborder="0"
+                frameBorder="0"
                 scrolling="no"
-                marginheight="0"
-                marginwidth="0"
+                marginHeight="0"
+                marginWidth="0"
                 src="https://maps.google.com/maps?width=100%25&amp;height=200&amp;hl=en&amp;q=Kharar%20chandigarh%20Punjab+(Your%20Business%20Name)&amp;t=k&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"
               ></iframe>
             </div>
